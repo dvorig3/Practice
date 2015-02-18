748cb5cd870ad772160dc57c78ed44a7dc1ac5cd
dvorig3 <idvorskij@mail.ru>
2015-02-18 02:38:11 +0200

    add validation

---
+++ Journal/src/main/java/idvorskij/labs/journal/entities/ErrorType.java
@@ -0,0 +1,5 @@
+package idvorskij.labs.journal.entities;
+
+public enum ErrorType {
+	FATAL_ERROR, SERIOUS_ERROR, NORMAL_OPERATION
+}
--- Journal/src/main/java/idvorskij/labs/journal/entities/Journal.java
+++ Journal/src/main/java/idvorskij/labs/journal/entities/Journal.java
@@ -1,10 +1,91 @@
 package idvorskij.labs.journal.entities;
+
+import java.sql.Timestamp;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 
 public class Journal {
-	@NotNull
-	private int id;
-	
-	
 
+	public Journal(@Min(0) int id, @NotNull ErrorType type,
+			@NotNull @Size(min = 5, max = 50) String name,
+			@NotNull @Size(min = 5, max = 256) String description,
+			@NotNull Timestamp date) {
+		this.id = id;
+		this.type = type;
+		this.name = name;
+		this.description = description;
+		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(@Min(0) int id) {
+		this.id = id;
+	}
+
+	public ErrorType getType() {
+		return type;
+	}
+
+	public void setType(@NotNull ErrorType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(@NotNull @Size(min = 5, max = 50) String name) {
+		this.name = name;
+	}
+
+	public java.sql.Timestamp getDate() {
+		return date;
+	}
+
+	public void setDate(@NotNull java.sql.Timestamp date) {
+		this.date = date;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(
+			@NotNull @Size(min = 5, max = 256) String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Journal [id=" + id + ", type=" + type + ", name=" + name
+				+ ", description=" + description + ", date=" + date + "]";
+	}
+
+	public static void main(String[] args) {
+		Journal j = new Journal(-9, ErrorType.FATAL_ERROR, null, "Description", new Timestamp(13));
+		System.out.println(j);
+	}
+	
+	@Min(0)
+	private int id;
+
+	@NotNull
+	private ErrorType type;
+
+	@NotNull(message="Имя должно быть задано")
+	@Size(min = 5, max = 50)
+	private String name;
+
+	@NotNull
+	@Size(min = 5, max = 256)
+	private String description;
+
+	@NotNull
+	private java.sql.Timestamp date;
 }
