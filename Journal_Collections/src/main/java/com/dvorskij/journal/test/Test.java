e30c70da3d8ab0edb309a90f99bf053dfa2a9ddd
dvorig3 <idvorskij@mail.ru>
2015-03-11 23:35:30 +0200

    test

--- Journal_Collections/src/main/java/com/dvorskij/journal/test/Test.java
+++ Journal_Collections/src/main/java/com/dvorskij/journal/test/Test.java
@@ -1,5 +1,18 @@
 package com.dvorskij.journal.test;
 
+import java.util.Random;
+import com.dvorskij.journal.services.CollectionJournal;
+import com.dvorskij.journal.services.Journal;
+
 public class Test {
+	
+	public static void main(String[] args) {
+		Random rand = new Random();
+		Journal j = new CollectionJournal();
+		for(int i = 0; i < 1000; i++){
+			
+		}
+		
+	}
 
 }
